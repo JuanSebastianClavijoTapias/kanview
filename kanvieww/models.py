d2953e631a8d52eff4bcaddf87b7ac4b2a6d34a7
SamuelRios1910 <samuel_pineres@soy.sena.edu.co>
2026-03-09 11:45:16 -0500

    Creacion de tabla Empleado

--- kanvieww/models.py
+++ kanvieww/models.py
@@ -10,3 +10,21 @@ class Cliente(models.Model):
     direccion = models.CharField(max_length=50)
     edad = models.IntegerField()
     
+class Empleado(models.Model):
+    documento = models.CharField(max_length=40)
+    nombre = models.CharField(max_length=100)
+    #tapiceros, costura, gerente, secretario
+    cargos = (
+        ("costr","Costurero"),
+        ("tapcr", "Tapicero"),
+        ("gerente","Gerente"),
+        ("secre", "Secretario")
+    )
+    cargo = models.CharField(choices=cargos, max_length=10, default="tapcr")
+    celular = models.CharField(max_length=20)
+    direccion = models.CharField(max_length=50)
+    salario = models.DecimalField()
+    edad = models.IntegerField()
+    fecha_nacimiento = models.DateField()
+    tareas_realizadas = models.IntegerField()
+    
