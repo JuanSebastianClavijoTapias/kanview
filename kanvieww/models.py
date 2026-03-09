c147e0f04a539f9776f62425e27c7bfd5b16036d
SamuelRios1910 <samuel_pineres@soy.sena.edu.co>
2026-03-09 11:33:28 -0500

    Creacion de tabla Clientes

--- kanvieww/models.py
+++ kanvieww/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Cliente(models.Model):
+    nombre = models.CharField(max_length=100)
+    fecha_nacimiento = models.DateField()
+    documento = models.CharField(max_length=20)
+    celular = models.CharField(max_length=20)
+    direccion = models.CharField(max_length=50)
+    edad = models.IntegerField()
+    
